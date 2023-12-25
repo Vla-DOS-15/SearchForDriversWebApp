1e7bb566a24c55be646d23f51de3ce520cf653d5
DeadInside <DeadInside@DESKTOP-POAUPA8>
2023-12-25 21:46:01 +0200

    Add project files.

---
+++ SearchForDriversWebApp/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchForDriversWebApp.Models;
+using System.Diagnostics;
+using SearchForDriversWebApp.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SearchForDriversWebApp.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Logging;
+using System.Net.Mail;
+using System.Net;
+
+namespace SearchForDriversWebApp.Controllers
+{
+    [Authorize(Roles = "Admin, User, Driver")]
+
+    public class HomeController : Controller
+    {
+        private DriverDbContext db;
+        // private TripService _tripService;
+        private readonly ILogger<HomeController> logger;
+        private readonly Services.EmailService emailService;
+
+        public HomeController(DriverDbContext db, ILogger<HomeController> logger, EmailService emailService)
+        {
+            this.db = db;
+            this.logger = logger;
+            this.emailService = emailService;
+        }
+
+        public async Task<IActionResult> Index()
+        {          
+
+            return View();
+        }
+
+        public IActionResult SendEmail()
+        {
+            //emailService.SendEmailDefault();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(TripModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.FirstOrDefault(x => x.Email == User.Identity.Name);
+                var trip = new Trip
+                {
+                    DepartureLocation = model.DepartureLocation,
+                    ArrivalLocation = model.ArrivalLocation,
+                    Distance = model.Distance,
+                    Status = "Опрацьовується оператором",
+                    UserId = user.Id
+                };
+                db.Trips.Add(trip);
+                db.SaveChanges();
+                emailService.SendEmailDefault(user.Email, $"Шановний {user.Username}, дякуємо за використання сервісу Yavir!\nВаша поїздка успішно створена.\nНаразі статус Вашого замовлення:{trip.Status}.\nОчікуйте оновлень статусу замовлення!");
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult AssignmentTrips()
+        {
+            var assignments = db.Assignments.Include(x=>x.User).Include(x=>x.Trip).Where(x=>x.User.Email == User.Identity.Name && x.User.Role == "Driver").ToList(); 
+            return View(assignments);
+        }
+
+        public IActionResult UserTrips()
+        {
+                var trips = db.Trips.Include(x => x.User).Where(x => x.User.Email == User.Identity.Name && x.User.Role == "User").ToList();
+                return View(trips);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult NotFoundPage()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+
+    }
+}
---
+++ SearchForDriversWebApp/Services/EmailService.cs
@@ -0,0 +1,70 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Net.Mail;
+using System.Net;
+
+namespace SearchForDriversWebApp.Services
+{
+    public class EmailService
+    {
+        private readonly ILogger<EmailService> logger;
+
+        public EmailService(ILogger<EmailService> logger)
+        {
+            this.logger = logger;
+        }
+
+        //System.Net.Mail.SmtpClient
+        public void SendEmailDefault(string sender, string messageText)
+        {
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Yavir Admin", "admin@mycompany.com"));
+                message.To.Add(new MailboxAddress("Recipient Name", $"{sender}"));
+                message.Subject = "Yavir";
+                message.Body = new TextPart("html") { Text = $"<div style=\"color: black;\">{messageText}</div>" };
+
+                using (var client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("bohdanets_ak20@nuwm.edu.ua", "june1506!");
+                    client.Send(message);
+                    client.Disconnect(true);
+                    logger.LogInformation("Сообщение отправлено успешно!");
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.GetBaseException().Message);
+            }
+        }
+
+        //MailKit.Net.Smtp.SmtpClient
+        //public void SendEmailCustom()
+        //{
+        //    try
+        //    {
+        //        MimeMessage message = new MimeMessage();
+        //        message.From.Add(new MailboxAddress("Моя компания", "admin@mycompany.com")); //отправитель сообщения
+        //        message.To.Add(new MailboxAddress("mail@yandex.ru")); //адресат сообщения
+        //        message.Subject = "Сообщение от MailKit"; //тема сообщения
+        //        message.Body = new BodyBuilder() { HtmlBody = "<div style=\"color: green;\">Сообщение от MailKit</div>" }.ToMessageBody(); //тело сообщения (так же в формате HTML)
+
+        //        using (MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient())
+        //        {
+        //            client.Connect("smtp.gmail.com", 587, true); //либо использум порт 465
+        //            client.Authenticate("mail@gmail.com", "secret"); //логин-пароль от аккаунта
+        //            client.Send(message);
+
+        //            client.Disconnect(true);
+        //            logger.LogInformation("Сообщение отправлено успешно!");
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        logger.LogError(e.GetBaseException().Message);
+        //    }
+        //}
+    }
+}
